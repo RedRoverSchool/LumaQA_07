b6077750842afdb5e553d92916b722ee542128c3
Andrii Koss <Lion7500000@gmail.com>
2023-11-16 18:49:15 -0800

    Update TC _006.013.001, TC _006.013.002, TC _006.013.003

--- tests/eco_collection_new_page/test_collection_page.py
+++ tests/eco_collection_new_page/test_collection_page.py
@@ -6,14 +6,14 @@ def test_title_page(driver):
     page = CollectionPage(driver, url=CollectionPage.URL)
     page.open()
     title = page.title()
-    assert title == CollectionPage.TITLE_TEXT, f'xpected text: {CollectionPage.TITLE_TEXT}, but got: {title}'
+    assert title == CollectionPage.TITLE_TEXT, f'Expected text: {CollectionPage.TITLE_TEXT}, but got: {title}'
 
 
 def test_banner_title(driver):
     page = CollectionPage(driver, url=CollectionPage.URL)
     page.open()
     banner_title = page.banner_title()
-    assert banner_title == CollectionPage.BANNER_TEXT, (f'xpected text: {CollectionPage.BANNER_TEXT}, '
+    assert banner_title == CollectionPage.BANNER_TEXT, (f'Expected text: {CollectionPage.BANNER_TEXT}, '
                                                         f'but got: {banner_title}')
 
 
@@ -21,4 +21,4 @@ def test_bunner_subtitle(driver):
     page = CollectionPage(driver, url=CollectionPage.URL)
     page.open()
     subtitle = page.banner_subtitle()
-    assert subtitle == CollectionPage.SUBTITLE_TEXT, f'xpected text: {CollectionPage.SUBTITLE_TEXT}, but got: {subtitle}'
+    assert subtitle == CollectionPage.SUBTITLE_TEXT, f'Expected text: {CollectionPage.SUBTITLE_TEXT}, but got: {subtitle}'
