871113e40f9a83b151ddffc077d9b207aa6632cb
KatyaMy <20FamGam20@gmail.com>
2023-11-17 19:42:12 -0500

    TC__001.001.003 | Main page > Self >Verify the presence of image boxes on the main page

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,11 +1,10 @@
+import time
+
 from pages.main_page import MainPage
 from locators.base_page_locators import BasePageLocators
 from pages.erin_recommends.erin_recommends import ErinRecommendsPage
 from data.men_page_url import MEN_PAGE
 from pages.performance_fabrics.performance_fabrics import PerformanceFabricsPage
-
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
 
 
 class TestMainPage:
@@ -24,7 +23,6 @@ class TestMainPage:
         page.open()
         page.check_clickability_of_erin_recommends_widget()
 
-
     def test_main_page_erin_recommends_is_clickable(self, driver):
         page = MainPage(driver, url=MainPage.URL)
         page.open()
@@ -39,7 +37,6 @@ class TestMainPage:
 
         assert driver.current_url == MEN_PAGE
 
-
     def test_main_page_erin_recommends_visible(self, driver):
         page = MainPage(driver, url=MainPage.URL)
         page.open()
@@ -47,11 +44,19 @@ class TestMainPage:
         page.is_visible(BasePageLocators.ERIN_SECTION)
         assert page.is_visible(BasePageLocators.ERIN_SECTION), "element is not visible"
 
-
     def test_main_page_shop_performance_is_clickable(self, driver):
         page = MainPage(driver, url=MainPage.URL)
         page.open()
         page.scroll_down_to_shop_performance()
         page.is_clickable(BasePageLocators.SHOP_PERFORMANCE).click()
         assert driver.current_url == PerformanceFabricsPage.URL
+
+    def test_presence_of_image_boxes(self, driver):
+        page = MainPage(driver, url=MainPage.URL)
+        page.open()
+        results = page.check_images_boxes_on_page()
+        assert all(results), "Not all blocks are present on the page"
+
+
+
 
