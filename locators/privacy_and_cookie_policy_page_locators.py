b67d961ca713e8ffae9b6079a408918914a5352b
evg-QA <freecadelka@yahoo.com>
2023-11-22 03:00:30 +0300

    TC_012.007.001 - TC_012.007.002| added: review - added: class PrivacyCookiePolicyPageLocators with the locators for both TC.

--- locators/privacy_and_cookie_policy_page_locators.py
+++ locators/privacy_and_cookie_policy_page_locators.py
@@ -1,2 +1,5 @@
-# block content "Your Choices Regarding Use Of The Information We Collect"
-YOUR_CHOICES_REGARDING_USE_OF_THE_INFORMATION_WE_COLLECT_CONTENT_LOCATOR = "//*[preceding::h2[text()='Your Choices Regarding Use Of The Information We Collect']][following::h2[text()='Your California Privacy Rights']]"
+
+#  block content "Your Choices Regarding Use Of The Information We Collect"
+class PrivacyCookiePolicyPageLocators:
+    BLOCK_CONTENT_HEADER_LOCATOR = "//h2[@id='privacy-policy-title-7']"
+    BLOCK_CONTENT_LOCATOR = "//*[preceding::h2[text()='Your Choices Regarding Use Of The Information We Collect']][following::h2[text()='Your California Privacy Rights']]"
