a256a953101c6c0a05d2fc72ff732ba26525ec6e
evg-QA <freecadelka@yahoo.com>
2023-11-22 03:08:14 +0300

    TC_012.007.003 | added: test_text_block_font_size_titled_cour_choices_regarding_use_of_the_information_we_collect

--- tests/privacy_and_cookie_policy_page/test_privacy_and_cookie_policy.py
+++ tests/privacy_and_cookie_policy_page/test_privacy_and_cookie_policy.py
@@ -21,3 +21,12 @@ def test_text_block_header_font_size_titled_cour_choices_regarding_use_of_the_in
     element = driver.find_element(By.XPATH, PrivacyCookiePolicyPageLocators.BLOCK_CONTENT_HEADER_LOCATOR)
     font_size = element.value_of_css_property('font-size')
     assert font_size == PrivacyCookiePolicyFonts.HEADER_TEXT_FONT_SIZE
+
+def test_text_block_font_size_titled_cour_choices_regarding_use_of_the_information_we_collect(driver):
+    """TC_012.007.003 | Footer > "Privacy and Cookie Policy" > Content >
+     The Font-size of the text of the block titled 'Your Choices Regarding Use Of The Information We Collect'"""
+    page = BasePage(driver, url=PRIVACY_AND_COOKIE_POLICY_PAGE)
+    page.open()
+    element = driver.find_element(By.XPATH, PrivacyCookiePolicyPageLocators.BLOCK_CONTENT_LOCATOR)
+    font_size = element.value_of_css_property('font-size')
+    assert font_size == PrivacyCookiePolicyFonts.TEXT_FONT_SIZE
