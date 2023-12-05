b2e5e245fce021e964c57a23b47105cf571724a1
elena <elena.ayrapetyan@gmail.com>
2023-12-04 20:22:41 -0800

    TC_014.001.002

--- locators/wish_list_locators.py
+++ locators/wish_list_locators.py
@@ -5,6 +5,6 @@ class WishListLocators:
     BUTTON_ALL_TO_CART = (By.XPATH, '//*[@class="action tocart"]')
     WISH_LIST_SIDEBAR = (By.ID, 'wishlist-sidebar')
     WISH_LIST_SIDEBAR_ITEMS = (By.CSS_SELECTOR, '#wishlist-sidebar .product-item')
-
-
-
+    SHARE_WISH_LIST_BUTTON = (By.XPATH, '//*[@title="Share Wish List"]')
+    EMAIL_ADDRESS_TEXT_BOX = (By.XPATH, '//*[@id="email_address"]')
+    MESSAGE_FIELD_TEXT_BOX = (By.XPATH, '//*[@id="message"]')
